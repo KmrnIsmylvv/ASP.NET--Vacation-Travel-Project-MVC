9d04f1655d722b383b2329b7a49a647c03a46796
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-02-02 15:25:37 +0400

    feat: created About page

---
+++ Vacation-Travel Site/Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vacation_Travel_Site.Models.Classes;
+
+namespace Vacation_Travel_Site.Controllers
+{
+    public class AboutController : Controller
+    {
+        Context context = new Context();
+        // GET: About
+        public ActionResult Index()
+        {
+            var variables = context.Abouts.ToList();
+            return View(variables);
+        }
+    }
+}
