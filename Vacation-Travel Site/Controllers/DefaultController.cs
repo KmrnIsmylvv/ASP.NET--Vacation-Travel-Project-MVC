3775cf5441d83aac4525c69bfd14a6511f892719
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-02-15 16:32:00 +0400

    feat: added slider and partial views into home page

--- Vacation-Travel Site/Controllers/DefaultController.cs
+++ Vacation-Travel Site/Controllers/DefaultController.cs
@@ -3,20 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Vacation_Travel_Site.Models.Classes;
 
 namespace Vacation_Travel_Site.Controllers
 {
     public class DefaultController : Controller
     {
+        Context context = new Context();
         // GET: Default
         public ActionResult Index()
         {
-            return View();
+            var blogs = context.Blogs.ToList();
+            return View(blogs);
         }
 
         public ActionResult About()
         {
             return View();
         }
+
+        public PartialViewResult PartialBlog()
+        {
+
+            var blogs = context.Blogs.OrderByDescending(b => b.Id).Take(2).ToList();
+            return PartialView(blogs);
+        }
+
+        //public PartialViewResult RightPartialBlog()
+        //{
+        //    var blogs = context.Blogs.Where(b=>b.Id==1)
+        //    return PartialView();
+        //}
     }
 }
