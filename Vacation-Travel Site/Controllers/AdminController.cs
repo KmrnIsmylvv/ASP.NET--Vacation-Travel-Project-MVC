7acf9127100eb58449445a800e5a1a2756d2b894
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-02-26 22:24:54 +0400

    feat: Listing Blogs in admin panel

--- Vacation-Travel Site/Controllers/AdminController.cs
+++ Vacation-Travel Site/Controllers/AdminController.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Vacation_Travel_Site.Models.Classes;
 
 namespace Vacation_Travel_Site.Controllers
 {
     public class AdminController : Controller
     {
+        Context context = new Context();
+
         // GET: Admin
         public ActionResult Index()
         {
-            return View();
+            var blogs = context.Blogs.ToList();
+
+            return View(blogs);
         }
     }
 }
