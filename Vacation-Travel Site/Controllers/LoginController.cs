fa2f80f479210c34a3a27d84ff7066f88cd07311
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-03-04 09:42:51 +0400

    feat: adding Login Panel

---
+++ Vacation-Travel Site/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vacation_Travel_Site.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
