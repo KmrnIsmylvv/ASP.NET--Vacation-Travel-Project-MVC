69c24c8afa116b1188e7214915a3ef2646a7f560
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-03-06 21:58:25 +0400

    feat: Login User

--- Vacation-Travel Site/Controllers/LoginController.cs
+++ Vacation-Travel Site/Controllers/LoginController.cs
@@ -3,15 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using Vacation_Travel_Site.Models.Classes;
 
 namespace Vacation_Travel_Site.Controllers
 {
     public class LoginController : Controller
     {
+        Context context = new Context();
         // GET: Login
         public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Admin admin)
+        {
+            var info = context.Admins.FirstOrDefault(a => a.Username == admin.Username && a.Password == admin.Password);
+
+            if (info != null)
+            {
+                FormsAuthentication.SetAuthCookie(info.Username, false);
+                Session["User"] = info.Username.ToString();
+                return RedirectToAction("Index", "Admin");
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
